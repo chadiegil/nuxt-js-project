8e18aabcb2cf31a37137c22b2ee541e47ee72aeb
unknown <chadiegilaugis@gmail.com>
2023-01-12 08:55:38 +0800

    pages and components added

---
+++ components/Car/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://cdn.pixabay.com/photo/2020/03/15/15/36/oldtimer-4933993_960_720.jpg')]"
+  >
+    <div class="z-10 absolute w-full h-full flex justify-center items-center">
+      <div class="text-center w-1/2 z-20">
+        <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+        <!-- Home search bar -->
+        <CarSearchBar />
+      </div>
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+    </div>
+  </div>
+</template>
